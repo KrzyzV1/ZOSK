c60304b55ed0a27344ecb59cbd9b1dd09cffeb1b
KrzyzV1 <damian20022@onet.pl>
2024-12-17 19:08:15 +0100

    Opis zmian

--- src/main/java/pl/zpi/zosk/baza/sterowniki/GenericController.java
+++ src/main/java/pl/zpi/zosk/baza/sterowniki/GenericController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -22,6 +23,17 @@ public abstract class GenericController<T> {
     @GetMapping
     public ResponseEntity<List<T>> getAll() {
         return ResponseEntity.ok(repository.getAll());
+    }
+
+    
+    @PostMapping
+    public ResponseEntity<String> save(@RequestBody List<T> entities) {
+        if (entities == null || entities.isEmpty()) {
+            return ResponseEntity.badRequest().body("No entities provided for saving.");
+        }
+
+        int savedCount = repository.save(entities);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCount + " entities saved successfully.");
     }
 
 
